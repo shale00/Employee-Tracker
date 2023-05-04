72fb435eb5aaf7c8126e577228ba85a864aefec5
sth <sth0067@gmail.com>
2023-05-04 15:05:17 -0500

    added updateEeRole function

--- index.js
+++ index.js
@@ -72,12 +72,21 @@ const viewAllEmployees = async () => {
     const allEmployees = await db.promise().query('SELECT employee.id, employee.first_name, employee.last_name, role.title AS role, department.name AS department, role.salary, CONCAT(mgr.first_name, " ", mgr.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT OUTER JOIN employee mgr ON employee.manager_id = mgr.id');
     console.table(allEmployees[0]);
     loadMainPrompt();
-}
+};
 
 const viewAllRoles = async () => {
     const allRoles = await db.promise().query('SELECT role.id AS role_id, role.title AS job_title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id');
     console.table(allRoles[0]);
     loadMainPrompt();
+};
+
+const viewAllDepartments = async () => {
+    const allDepartments = await db.promise().query('SELECT ')
+}
+
+const departmentList = async () => {
+    const departments = await db.promise().query('SELECT name AS department, id AS value FROM department');
+    return departments;
 }
 
 const employeeList = async () => {
@@ -85,12 +94,12 @@ const employeeList = async () => {
     const employeeArr = employees[0];
     employeeArr.push({value: null, name: 'None'});
     return employeeArr;
-}
+};
 
 const roleList = async () => {
     const roles = await db.promise().query('SELECT title AS name, id AS value FROM role');
     return roles[0];
-}
+};
 
 //Nested question prompt functions
 const addEmployeePrompt = async () => {
@@ -135,7 +144,7 @@ const addEmployeePrompt = async () => {
         }
     ]);
         await db.promise().query('INSERT INTO employee SET ?', answers);
-            viewAllEmployees();
+        viewAllEmployees();
 };
 
 const updateEmployeeRolePrompt = async () => {
@@ -154,7 +163,35 @@ const updateEmployeeRolePrompt = async () => {
         }
     ]);
         await db.promise().query('UPDATE employee SET role_id = ? WHERE id = ?', [answers.role_id, answers.id]);
-            viewAllEmployees();
+        viewAllEmployees();
+};
+
+const addRolePrompt = async () => {
+    const answers = await prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of the role?',
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of the role?'
+        },
+        {
+            type: 'list',
+            name: 'department_id',
+            message: 'Which department does the role belong to?',
+            choices: departmentList
+        }
+    ]);
+        await db.promise().query('INSERT INTO role (title, salary, department_id) VALUES (?)', answers);
+        viewAllRoles();
+
+}
+
+const addDepartmentPrompt = async () => {
+
 }
 
 
